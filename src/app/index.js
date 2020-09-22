2229a78bca6b6e706a7f51d258639351f0215bf0
Edward Iga Kigongo <igakigongo@gmail.com>
2020-09-22 16:40:43 +0200

    update partner tab form

--- src/app/index.js
+++ src/app/index.js
@@ -9,7 +9,7 @@ const App = () => {
     <div className="App">
       <Header />
       <Main />
-      <PartnetTabForm />
+      {/* <PartnetTabForm /> */}
       <Footer />
     </div>
   );
--- src/partner/partner-tab-form.js
+++ src/partner/partner-tab-form.js
@@ -2,15 +2,13 @@ import React from 'react';
 import PartnetPersonForm from './partner-person-form';
 import PartnetOrgForm from './partner-organization-form';
 
-
-class PartnetTabForm extends React.Component{
+class PartnetTabForm extends React.Component {
   render() {
     return (
-        <div>
-            <PartnetPersonForm isVisable={true} />
-            <PartnetOrgForm isVisable={false} />
-        </div>
-      
+      <>
+        <PartnetPersonForm isVisable={true} />
+        <PartnetOrgForm isVisable={false} />
+      </>
     );
   }
 };
