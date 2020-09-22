cc586a9beb53fb523bb800aa48d00509fbe3e3ec
Edward Iga Kigongo <igakigongo@gmail.com>
2020-09-22 13:52:27 +0200

    add about-us section, install prop-types

--- src/app/index.js
+++ src/app/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import PartnetTabForm from '../partner/partner-tab-form';
 import Header from '../header/header';
 import Footer from '../footer/footer';
-import IntroductionSection from '../intro-section';
+import Main from '../content/main';
 
 const App = () => {
   return (
     <div className="App">
       <Header />
-      <IntroductionSection />
+      <Main />
       <PartnetTabForm />
       <Footer />
     </div>
---
+++ src/intro-section/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const IntroductionSection = () => <section id="intro" class="clearfix">
+  <div class="container">
+    <div class="intro-info">
+      <h2>Empowering young women and girls in Africa!</h2>
+      <div>
+        <a href="#about" class="btn-get-started scrollto">Get Started</a>
+        <a href="#services" class="btn-services scrollto">Our Services</a>
+      </div>
+    </div>
+
+  </div>
+</section>;
+
+export default IntroductionSection;
