1539eaf48338d335eece8795af7c374dad9a2e35
Edward Iga Kigongo <igakigongo@gmail.com>
2020-09-22 13:30:43 +0200

    add header and css

--- src/app/index.js
+++ src/app/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import PartnetTabForm from '../partner/partner-tab-form';
 import Header from '../header/header';
 import Footer from '../footer/footer';
+import IntroductionSection from '../intro-section';
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <IntroductionSection />
       <PartnetTabForm />
       <Footer />
     </div>
