a06dc5b680aaa2a5d11d9e87d25baf672b04fcfe
Edward Iga Kigongo <igakigongo@gmail.com>
2020-09-22 14:34:00 +0200

    add why-us section

--- src/content/home-page/index.js
+++ src/content/home-page/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import AboutUs from './about-us-section';
 import IntroductionSection from './introduction-section';
 import Services from './services-section';
+import WhyUs from './why-us-section';
 
 const HomePage = () => <>
   <IntroductionSection />
   <main id="main">
     <AboutUs />
     <Services />
+    <WhyUs />
   </main>
 </>;
 
