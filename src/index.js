4f79b1c0516350db8488bb5923ebf2e99ed319fa
Yazan Kshiek <ksh.yazan.mail.face@gmail.com>
2020-09-23 23:58:08 +0400

    run react app after merge

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
-import App from "./app";
+import App from "./app/index";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.css";
 
