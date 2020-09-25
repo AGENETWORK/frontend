55a2fdc9a6abfc353931ccfcf0057c46e313e938
Edward Iga Kigongo <igakigongo@gmail.com>
2020-09-25 05:19:42 +0200

    add owl.carousel, update testimonials section

--- src/content/home-page/testimonials-section.js
+++ src/content/home-page/testimonials-section.js
@@ -1,95 +1,92 @@
 /* eslint-disable max-len */
-import React from "react";
+/* eslint-disable react/no-array-index-key */
+import React, { useEffect } from "react";
+import $ from "jquery";
+import "owl.carousel.es6";
 
-const Testimonials = () => (
-  <section id="testimonials" className="section-bg">
-    <div className="container">
-      <header className="section-header">
-        <h3>Testimonials</h3>
-      </header>
+const Testimonials = () => {
+  const baseUrl = "https://res.cloudinary.com/temi2015/image/upload";
+  const testimonials = [
+    {
+      idx: 0,
+      imgSrc: `${baseUrl}/v1599566790/AGENETWORK/testimonial-1_zintb3.jpg`,
+      name: "Saul Goodman",
+      title: "CEO & Founder",
+      remarks: `Proin iaculis purus consequat sem cure digni ssim donec porttitora entum 
+      suscipit rhoncus. Accusantium quam, ultricies eget id, aliquam eget nibh et. 
+      Maecen aliquam, risus at semper.`,
+    },
+    {
+      idx: 1,
+      imgSrc: `${baseUrl}/v1599566793/AGENETWORK/testimonial-2_x54pzu.jpg`,
+      name: "Sara Wilsson",
+      title: "Designer",
+      remarks: `Export tempor illum tamen malis malis eram quae irure esse labore quem cillum 
+      quid cillum eram malis quorum velit fore eram velit sunt aliqua noster fugiat 
+      irure amet legam anim culpa.`,
+    },
+    {
+      idx: 2,
+      imgSrc: `${baseUrl}/v1599566789/AGENETWORK/testimonial-3_bdhkkz.jpg`,
+      name: "Jena Karlis",
+      title: "Store Owner",
+      remarks: `Enim nisi quem export duis labore cillum quae magna enim sint quorum nulla quem
+      veniam duis minim tempor labore quem eram duis noster aute amet eram fore quis sint
+      minim.`,
+    },
+    {
+      idx: 3,
+      imgSrc: `${baseUrl}/v1599566788/AGENETWORK/testimonial-4_aank4v.jpg`,
+      name: "Matt Brandon",
+      title: "Freelancer",
+      remarks: `Fugiat enim eram quae cillum dolore dolor amet nulla culpa multos export minim
+      fugiat minim velit minim dolor enim duis veniam ipsum anim magna sunt elit fore quem
+      dolore labore illum veniam.`,
+    },
+    {
+      idx: 4,
+      imgSrc: `${baseUrl}/v1599566793/AGENETWORK/testimonial-5_lh2irg.jpg`,
+      name: "John Larson",
+      title: "Entrepreneur",
+      remarks: `Quis quorum aliqua sint quem legam fore sunt eram irure aliqua veniam tempor noster
+      veniam enim culpa labore duis sunt culpa nulla illum cillum fugiat legam esse veniam
+      culpa fore nisi cillum quid.`,
+    },
+  ];
 
-      <div className="row justify-content-center">
-        <div className="col-lg-8">
-          <div className="owl-carousel testimonials-carousel wow fadeInUp">
-            <div className="testimonial-item">
-              <img
-                src="https://res.cloudinary.com/temi2015/image/upload/v1599566790/AGENETWORK/testimonial-1_zintb3.jpg"
-                className="testimonial-img"
-                alt=""
-              />
-              <h3>Saul Goodman</h3>
-              <h4>Ceo &amp; Founder</h4>
-              <p>
-                Proin iaculis purus consequat sem cure digni ssim donec porttitora entum suscipit
-                rhoncus. Accusantium quam, ultricies eget id, aliquam eget nibh et. Maecen aliquam,
-                risus at semper.
-              </p>
-            </div>
+  useEffect(() => {
+    $(".testimonials-carousel").owlCarousel({
+      autoplay: true,
+      dots: true,
+      loop: true,
+      items: 1,
+    });
+  }, []);
 
-            <div className="testimonial-item">
-              <img
-                src="https://res.cloudinary.com/temi2015/image/upload/v1599566793/AGENETWORK/testimonial-2_x54pzu.jpg"
-                className="testimonial-img"
-                alt=""
-              />
-              <h3>Sara Wilsson</h3>
-              <h4>Designer</h4>
-              <p>
-                Export tempor illum tamen malis malis eram quae irure esse labore quem cillum quid
-                cillum eram malis quorum velit fore eram velit sunt aliqua noster fugiat irure amet
-                legam anim culpa.
-              </p>
-            </div>
+  return (
+    <section id="testimonials" className="section-bg">
+      <div className="container">
+        <header className="section-header">
+          <h3>Testimonials</h3>
+        </header>
 
-            <div className="testimonial-item">
-              <img
-                src="https://res.cloudinary.com/temi2015/image/upload/v1599566789/AGENETWORK/testimonial-3_bdhkkz.jpg"
-                className="testimonial-img"
-                alt=""
-              />
-              <h3>Jena Karlis</h3>
-              <h4>Store Owner</h4>
-              <p>
-                Enim nisi quem export duis labore cillum quae magna enim sint quorum nulla quem
-                veniam duis minim tempor labore quem eram duis noster aute amet eram fore quis sint
-                minim.
-              </p>
-            </div>
-
-            <div className="testimonial-item">
-              <img
-                src="https://res.cloudinary.com/temi2015/image/upload/v1599566788/AGENETWORK/testimonial-4_aank4v.jpg"
-                className="testimonial-img"
-                alt=""
-              />
-              <h3>Matt Brandon</h3>
-              <h4>Freelancer</h4>
-              <p>
-                Fugiat enim eram quae cillum dolore dolor amet nulla culpa multos export minim
-                fugiat minim velit minim dolor enim duis veniam ipsum anim magna sunt elit fore quem
-                dolore labore illum veniam.
-              </p>
-            </div>
-
-            <div className="testimonial-item">
-              <img
-                src="https://res.cloudinary.com/temi2015/image/upload/v1599566793/AGENETWORK/testimonial-5_lh2irg.jpg"
-                className="testimonial-img"
-                alt=""
-              />
-              <h3>John Larson</h3>
-              <h4>Entrepreneur</h4>
-              <p>
-                Quis quorum aliqua sint quem legam fore sunt eram irure aliqua veniam tempor noster
-                veniam enim culpa labore duis sunt culpa nulla illum cillum fugiat legam esse veniam
-                culpa fore nisi cillum quid.
-              </p>
+        <div className="row justify-content-center">
+          <div className="col-lg-8">
+            <div className="owl-carousel testimonials-carousel wow fadeInUp">
+              {testimonials.map(({ imgSrc, name, title, remarks, idx }) => (
+                <div className="testimonial-item" key={idx}>
+                  <img alt="" className="testimonial-img" src={imgSrc} />
+                  <h3>{name}</h3>
+                  <h4>{title}</h4>
+                  <p>{remarks}</p>
+                </div>
+              ))}
             </div>
           </div>
         </div>
       </div>
-    </div>
-  </section>
-);
+    </section>
+  );
+};
 
 export default Testimonials;
